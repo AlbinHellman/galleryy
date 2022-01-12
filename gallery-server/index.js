e947ace27828c446466f8ec3131b52cd561753a0
AlbinHellman <steelalbin@gmail.com>
2022-01-12 17:52:18 +0100

    galleryy

---
+++ gallery-server/index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer((req, res) => {
+   // console.log(req);
+   //console.log(req.url, req.method)
+   
+   res.setHeader('Content-type', 'text/html')
+
+   fs.readFile('./views/index.html', (err, data) => {
+       if(err) {
+           console.log(err)
+           res.end()
+       }
+       res.end(data);
+   })
+})
+
+const PORT = process.env.PORT || 9999;
+
+server.listen(PORT, 'localhost', () => {
+    console.log('Server running at http://localhost:' + PORT)
+})
