5c3c72ab5bbb2137cd3f5fc8ea898158372d402f
AlbinHellman <steelalbin@gmail.com>
2022-02-14 15:20:56 +0100

    galleryy

--- gallery-server/index.js
+++ gallery-server/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 
 
@@ -9,43 +10,42 @@ const server = http.createServer((req, res) => {
    
    res.setHeader('Content-type', 'text/html')
  
-   
-      
-   
-   fs.readFile('./views/index.html', (err, data) => {
-       if(err) {
-           console.log(err)
-
-
-          
-           
-           res.end()
-       
-        
-       }
-       
-
-       
-  
+  // fs.readFile('./views/index.html', (err, data) => {
+     //  if(err) {
+     //      console.log(err)
+     //
+    //       res.end()
+    //   }
     
-       res.end(data);
-
-      
-
-      
+   //    res.end(data);
     
-   })
+ //  })
 
-   
+ let url;
+ switch(req.url) {
+     case '/':
+         url = 'index.html';
+         break;
+         case '/AboutUs':
+             url = 'AboutUs.html';
+             break;
+             default:
+                 url = '404.html';
+                 break;
+ }
 
+ let filePath = path.join(__dirname, 'views', url);
+
+ fs.readFile(filePath, (err, data) => {
+     if(err) {
+         console.log(err)
+         res.end()
+     }
+     res.end(data)
+ })
    
 
 })
-
-
-
-
-
 
 
 const PORT = process.env.PORT || 9999;
